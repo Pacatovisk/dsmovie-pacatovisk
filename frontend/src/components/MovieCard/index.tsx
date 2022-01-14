e4c965f4860ceb0570f75ad7ffe914a5bbf66220
Pacatovisck <marcelovianadiaspacato@gmail.com.br>
2022-01-14 10:33:30 -0300

    Show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -15,7 +15,7 @@ function MovieCard( { movie } : Props ) {
             <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="dsmovie-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
+                <MovieScore score={movie.score} count={movie.count} />
 
                 <Link to={`/form/${movie.id}`}>
                      <div className="btn btn-primary dsmovie-btn">Avaliar</div> 
